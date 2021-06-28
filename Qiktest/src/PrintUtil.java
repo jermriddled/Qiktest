baf6b844aa74189dd293a61f33a63d573a919922
jermriddled <46093224+jermriddled@users.noreply.github.com>
2021-06-28 13:29:25 -0700

    Add files via upload
    Updated print methods to return a StringBuilder object instead of just printing to console

--- Qiktest/src/PrintUtil.java
+++ Qiktest/src/PrintUtil.java
@@ -3,16 +3,26 @@ import java.text.NumberFormat;
 
 public class PrintUtil {
 
-    // Prints total number of times each category in each metric is satisfied
-    public static void printResults() {
+    private static StringBuilder allResults;
 
-        printOmissionResults();
-        printCommissionResults();
-        printVariabilityResults();
+    // String that contains total number of times each category in each metric is satisfied
 
+    public static StringBuilder getAllResults() {
+        createStringBuilder();
+        return allResults;
     }
 
-    private static void printOmissionResults() {
+    private static void createStringBuilder() {
+        allResults = new StringBuilder();
+        allResults.append(System.getProperty("line.separator"))
+                .append("# of patients evaluated: ").append(DataUtil.numberOfPatients)
+                .append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
+        appendOmissionResults();
+        appendCommissionResults();
+        appendVariabilityResults();
+    }
+
+    private static void appendOmissionResults() {
 
         int omissionIncreasesCategoryB = 0;
         int omissionDecreasesCategoryB = 0;
@@ -32,27 +42,45 @@ public class PrintUtil {
                 DataUtil.categoryD_Omissions.size() + DataUtil.categoryE_Omissions.size();
         int omissionDecreasesTotal = DataUtil.categoryA_Omissions.size() + omissionDecreasesCategoryB +
                 omissionDecreasesCategoryC;
-        System.out.println();
-        System.out.println("# of patients evaluated: " + DataUtil.numberOfPatients);
-        System.out.println("OMISSIONS:");
-        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Omissions.size());
-        System.out.println("Scores 90+ increased by 12+: " + omissionIncreasesCategoryB);
-        System.out.println("Scores 90+ decreased by 12+: " + omissionDecreasesCategoryB);
-        System.out.println("Scores between 78 and 90 increased by 8+: " + omissionIncreasesCategoryC);
-        System.out.println("Scores between 78 and 90 decreased by 8+: " + omissionDecreasesCategoryC);
-        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Omissions.size());
-        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Omissions.size());
-        System.out.println("% of significant increases: " + formatter.format(
+
+
+        allResults.append("OMISSIONS:").append(System.getProperty("line.separator"));
+        allResults.append("Scores of 115+, decreased by 15+: ").append(DataUtil.categoryA_Omissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ increased by 12+: ").append(omissionIncreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ decreased by 12+: ").append(omissionDecreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 increased by 8+: ").append(omissionIncreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 decreased by 8+: ").append(omissionDecreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 90 increased to 90+ and by 8+: ")
+                .append(DataUtil.categoryD_Omissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 70 increased to 78+: ").append(DataUtil.categoryE_Omissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("% of significant increases: ").append(formatter.format(
                 (omissionIncreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
-        System.out.println("% of significant decreases: " + formatter.format(
+                        * 100)).append("%").append(System.getProperty("line.separator"));
+        allResults.append("% of significant decreases: ").append(formatter.format(
                 (omissionDecreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
-        System.out.println();
+                        * 100)).append("%").append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
+
+//        System.out.println();
+//        System.out.println("# of patients evaluated: " + DataUtil.numberOfPatients);
+//        System.out.println("OMISSIONS:");
+//        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Omissions.size());
+//        System.out.println("Scores 90+ increased by 12+: " + omissionIncreasesCategoryB);
+//        System.out.println("Scores 90+ decreased by 12+: " + omissionDecreasesCategoryB);
+//        System.out.println("Scores between 78 and 90 increased by 8+: " + omissionIncreasesCategoryC);
+//        System.out.println("Scores between 78 and 90 decreased by 8+: " + omissionDecreasesCategoryC);
+//        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Omissions.size());
+//        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Omissions.size());
+//        System.out.println("% of significant increases: " + formatter.format(
+//                (omissionIncreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
+//        System.out.println("% of significant decreases: " + formatter.format(
+//                (omissionDecreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
+//        System.out.println();
 
     }
 
-    private static void printCommissionResults() {
+    private static void appendCommissionResults() {
 
         int commissionIncreasesCategoryB = 0;
         int commissionDecreasesCategoryB = 0;
@@ -72,25 +100,43 @@ public class PrintUtil {
                 DataUtil.categoryD_Commissions.size() + DataUtil.categoryE_Commissions.size();
         int commissionDecreasesTotal = DataUtil.categoryA_Commissions.size() + commissionDecreasesCategoryB +
                 commissionDecreasesCategoryC;
-        System.out.println("COMMISSIONS:");
-        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Commissions.size());
-        System.out.println("Scores 90+ increased by 12+: " + commissionIncreasesCategoryB);
-        System.out.println("Scores 90+ decreased by 12+: " + commissionDecreasesCategoryB);
-        System.out.println("Scores between 78 and 90 increased by 8+: " + commissionIncreasesCategoryC);
-        System.out.println("Scores between 78 and 90 decreased by 8+: " + commissionDecreasesCategoryC);
-        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Commissions.size());
-        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Commissions.size());
-        System.out.println("% of significant increases: " + formatter.format(
+
+        allResults.append("COMMISSIONS:").append(System.getProperty("line.separator"));
+        allResults.append("Scores of 115+, decreased by 15+: ").append(DataUtil.categoryA_Commissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ increased by 12+: ").append(commissionIncreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ decreased by 12+: ").append(commissionDecreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 increased by 8+: ").append(commissionIncreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 decreased by 8+: ").append(commissionDecreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 90 increased to 90+ and by 8+: ")
+                .append(DataUtil.categoryD_Commissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 70 increased to 78+: ").append(DataUtil.categoryE_Commissions.size()).append(System.getProperty("line.separator"));
+        allResults.append("% of significant increases: ").append(formatter.format(
                 (commissionIncreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
-        System.out.println("% of significant decreases: " + formatter.format(
+                        * 100)).append("%").append(System.getProperty("line.separator"));
+        allResults.append("% of significant decreases: ").append(formatter.format(
                 (commissionDecreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
-        System.out.println();
+                        * 100)).append("%").append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
+
+
+//        System.out.println("COMMISSIONS:");
+//        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Commissions.size());
+//        System.out.println("Scores 90+ increased by 12+: " + commissionIncreasesCategoryB);
+//        System.out.println("Scores 90+ decreased by 12+: " + commissionDecreasesCategoryB);
+//        System.out.println("Scores between 78 and 90 increased by 8+: " + commissionIncreasesCategoryC);
+//        System.out.println("Scores between 78 and 90 decreased by 8+: " + commissionDecreasesCategoryC);
+//        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Commissions.size());
+//        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Commissions.size());
+//        System.out.println("% of significant increases: " + formatter.format(
+//                (commissionIncreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
+//        System.out.println("% of significant decreases: " + formatter.format(
+//                (commissionDecreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
+//        System.out.println();
 
     }
 
-    private static void printVariabilityResults() {
+    private static void appendVariabilityResults() {
 
         int varIncreasesCategoryB = 0;
         int varDecreasesCategoryB = 0;
@@ -110,20 +156,38 @@ public class PrintUtil {
                 DataUtil.categoryD_Var.size() + DataUtil.categoryE_Var.size();
         int varDecreasesTotal = DataUtil.categoryA_Var.size() + varDecreasesCategoryB +
                 varDecreasesCategoryC;
-        System.out.println("VARIABILITY:");
-        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Var.size());
-        System.out.println("Scores 90+ increased by 12+: " + varIncreasesCategoryB);
-        System.out.println("Scores 90+ decreased by 12+: " + varDecreasesCategoryB);
-        System.out.println("Scores between 78 and 90 increased by 8+: " + varIncreasesCategoryC);
-        System.out.println("Scores between 78 and 90 decreased by 8+: " + varDecreasesCategoryC);
-        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Var.size());
-        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Var.size());
-        System.out.println("% of significant increases: " + formatter.format(
+
+        allResults.append("VARIABILITY:").append(System.getProperty("line.separator"));
+        allResults.append("Scores of 115+, decreased by 15+: ").append(DataUtil.categoryA_Var.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ increased by 12+: ").append(varIncreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores 90+ decreased by 12+: ").append(varDecreasesCategoryB).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 increased by 8+: ").append(varIncreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores between 78 and 90 decreased by 8+: ").append(varDecreasesCategoryC).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 90 increased to 90+ and by 8+: ")
+                .append(DataUtil.categoryD_Var.size()).append(System.getProperty("line.separator"));
+        allResults.append("Scores below 70 increased to 78+: ").append(DataUtil.categoryE_Var.size()).append(System.getProperty("line.separator"));
+        allResults.append("% of significant increases: ").append(formatter.format(
                 (varIncreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
-        System.out.println("% of significant decreases: " + formatter.format(
+                        * 100)).append("%").append(System.getProperty("line.separator"));
+        allResults.append("% of significant decreases: ").append(formatter.format(
                 (varDecreasesTotal / DataUtil.numberOfPatients)
-                        * 100) + "%");
+                        * 100)).append("%");
+
+
+//        System.out.println("VARIABILITY:");
+//        System.out.println("Scores of 115+, decreased by 15+: " + DataUtil.categoryA_Var.size());
+//        System.out.println("Scores 90+ increased by 12+: " + varIncreasesCategoryB);
+//        System.out.println("Scores 90+ decreased by 12+: " + varDecreasesCategoryB);
+//        System.out.println("Scores between 78 and 90 increased by 8+: " + varIncreasesCategoryC);
+//        System.out.println("Scores between 78 and 90 decreased by 8+: " + varDecreasesCategoryC);
+//        System.out.println("Scores below 90 increased to 90+ and by 8+: " + DataUtil.categoryD_Var.size());
+//        System.out.println("Scores below 70 increased to 78+: " + DataUtil.categoryE_Var.size());
+//        System.out.println("% of significant increases: " + formatter.format(
+//                (varIncreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
+//        System.out.println("% of significant decreases: " + formatter.format(
+//                (varDecreasesTotal / DataUtil.numberOfPatients)
+//                        * 100) + "%");
 
     }
 }
